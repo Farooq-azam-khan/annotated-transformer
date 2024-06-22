6b434e971fe45cf2047c59ce1119b265c4bf1bbc
farooq-azam-khan <khanazamfarooq99@gmail.com>
2024-06-22 18:24:35 -0400

    at decoder class fn implementation

--- model.py
+++ model.py
@@ -1,22 +1,26 @@
 import os
 import math
-import copy  # TODO: looup copy doc
+import copy
 import time
-import altair as alt  # TODO: what is this?
+import altair as alt  # https://altair-viz.github.io/getting_started/overview.html#overview
 import pandas as pd
 import spacy
-import GPUtil  # TODO: what does this lib do?
+
+# https://github.com/anderskm/gputil
+import GPUtil
 import warnings
 
 # TORCH IMPORTS
 import torch
 import torch.nn as nn
-from torch.nn.functional import log_softmax, pad  # TODO: lookup pad doc
-from torch.optim.lr_scheduler import LambdaLR  # TODO: how does this work?
+from torch.nn import functional as F
+from torch.optim.lr_scheduler import LambdaLR
+
+# Torch Utils
 from torch.utils.data import DataLoader
 from torch.utils.data.distributed import (
-    DistributedSampler,
-)  # TODO: what is a distrbuted sampler?
+    DistributedSampler,  # restricts data loading to a subset of the dataset
+)
 import torch.distributed as dist
 import torch.multiprocessing as mp
 from torch.nn.parallel import DistributedDataParallel as DDP
@@ -39,7 +43,6 @@ def show_example(fn, args=[]):
 class DummyOptimizer(torch.optim.Optimizer):
     def __init__(self):
         self.param_groups = [{"lr": 0}]
-        None
 
     def step(self):
         None
@@ -51,6 +54,9 @@ class DummyOptimizer(torch.optim.Optimizer):
 class DummyScheduler:
     def step(self):
         None
+
+
+# tgt === target
 
 
 class EncoderDecoder(nn.Module):
@@ -73,25 +79,29 @@ class EncoderDecoder(nn.Module):
 
 
 class Generator(nn.Module):
-    def __init_(self, d_model, vocab):
+    def __init__(self, d_model, vocab):
+        super(Generator, self).__init__()
         self.proj = nn.Linear(d_model, vocab)
 
     def forward(self, x):
-        return log_softmax(self.proj(x), dim=-1)
+        return F.log_softmax(self.proj(x), dim=-1)
 
 
 def clones(module, N):
-    return nn.ModuleList(
-        [copy.deepcopy(module) for _ in range(N)]
-    )  # N identical layers
+    return nn.ModuleList([copy.deepcopy(module) for _ in range(N)])
 
 
 class LayerNorm(nn.Module):
-    def __init__(self):
-        pass
+    def __init__(self, features, eps=1e-6):
+        super(LayerNorm, self).__init__()
+        self.a_2 = nn.Parameter(torch.ones(features))
+        self.b_2 = nn.Parameter(torch.zeros(features))
+        self.eps = eps
 
-    def forward(self):
-        pass
+    def forward(self, x):
+        mean = x.mean(-1, keepdim=True)
+        std = x.std(-1, keepdim=True)
+        return self.a_2 * (x - mean) / (std + self.eps) + self.b_2
 
 
 class Encoder(nn.Module):
@@ -103,6 +113,41 @@ class Encoder(nn.Module):
     def forward(self, x, mask):
         for layer in self.layers:
             x = layer(x, mask)
+        return self.norm(x)
+
+
+class SublayerConnection(nn.Module):
+    def __init__(self, size, dropout):
+        super(SublayerConnection, self).__init__()
+        self.norm = LayerNorm(size)
+        self.dropout = nn.Dropout(dropout)
+
+    def forward(self, x, sublayer):
+        return x + self.dropout(sublayer(self.norm(x)))
+
+
+class EncoderLayer(nn.Module):
+    def __init__(self, size, self_attn, feed_forward, dropout):
+        super(EncoderLayer, self).__init__()
+        self.self_attn = self_attn
+        self.feed_forward = feed_forward
+        self.sublayer = clones(SublayerConnection(size, dropout), 2)
+        self.size = size
+
+    def forward(self, x, mask):
+        x = self.sublayer[0](x, lambda x: self.self_attn(x, x, x, mask))
+        return self.sublayer[1](x, self.feed_forward)
+
+
+class Decoder(nn.Module):
+    def __init__(self, layer, N):
+        super(Decoder, self).__init__()
+        self.layers = clones(layer, N)
+        self.norm = LayerNorm(layer.size)
+
+    def forward(self, x, memory, src_mask, tgt_mask):
+        for layer in self.layers:
+            x = layer(x, memory, src_mask, tgt_mask)
         return self.norm(x)
 
 
